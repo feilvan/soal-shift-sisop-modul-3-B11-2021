0de81e3ddf6b556937732dd2228d29e12bee5335
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2021-05-08 22:00:01 +0700

    Create soal3.c

---
+++ soal3/soal3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+if (strcmp(argv[1],"-f")==0){  
+    int sum= 2, run = 2;
+    while (sum < argc) {
+      pthread_create(&(thread[jumlah]), NULL, myFile, (char*)argv[jumlah]);
+      sum++;
+    ())
+    while (jalankan < argc) {
+      pthread_join(thread[jalankan],NULL);
+    sum++;
+    }
+}
